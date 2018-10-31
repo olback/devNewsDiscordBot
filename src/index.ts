7686af12f48342fc8f1977a079e5a8d3e6c7d920
Skayo <jonasg.cool@gmail.com>
2018-10-31 21:50:45 +0100

    Changed everything to LowDB and added support for 2k or more characters posts

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,43 @@ import * as Discord from 'discord.js';
 import { env } from 'process';
 import * as dotenv from 'dotenv';
 import * as rantscript from 'rantscript';
+import * as lowdb from 'lowdb';
+import * as FileSync from 'lowdb/adapters/FileSync';
 
 interface Post {
-    msg: Discord.Message;
-    tags: string[];
-    img: string;
+	msg: Discord.Message;
+	tags: string[];
+	img: string;
 }
 
 dotenv.load({ path: '.env' }); // Load .env
+
 const cmdPrefix = env.COMMAND_PREFIX || '!dnb';
 const client = new Discord.Client();
-let posts: Post[] = [];
-let lastPost: Post | null;
 
-function helpText() {
-    return `
+const adapter = new FileSync('db.json');
+const db = lowdb(adapter);
+
+db.defaults({ users: [], queue: [] })
+  .write();
+
+function helpText (queue) {
+	return `
 **devNews Bot Commands Help:**
-publish - Add current article to the waitlist.
-length - The amount of unpublished news articles.
-show <n | current> - Show Article with index n-1 or the current one.
-tags <tag1> <tag2> <tags...> - Add tags to the current article.
-reset - Delete the current article. (text, tags)
-help - Show this help message.
+!publish - Add current article to the waitlist.
+!length - The amount of unpublished news articles.
+!show <n | current> - Show Article with index n-1 or the current one.
+!tags <tag1> <tag2> <tags...> - Add tags to the current article.
+!reset - Delete the current article. (text, tags)
+!signature <signature ...> - Set your own signature that gets posted as a comment on every of your posts.
+!help - Show this help message.
 
 Re-try / Post frequency: ${env.POST_FREQUENCY || 10} minutes.
-Articles in queue: ${posts.length} (max: 10).
+Articles in queue: ${queue.length} (max: 10).
 `;
 }
 
+/*
 function sendMessage(msg: string) {
 
     const channel = client.channels.get(env.RELEASE_CHANNEL_ID || '');
@@ -40,191 +49,280 @@ function sendMessage(msg: string) {
     }
 
 }
+*/
 
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('rateLimit', () => {
-    console.log('Well, shit...');
+	console.log('Well, shit...');
 });
 
+/*
 client.on('messageUpdate', (_msg, newMsg) => {
 
-    // console.log(msg.id, newMsg.id);
-    // console.log('Edit:', newMsg.content);
+	//console.log(msg.id, newMsg.id);
+	//console.log('Edit:', newMsg.content);
 
-    if (lastPost && lastPost.msg.id === newMsg.id) {
-        lastPost.msg = newMsg;
-    }
+
+	if (lastPost && lastPost.msg.author.id === newMsg.id) {
+		lastPost.msg = newMsg;
+	}
 
 });
+*/
 
 client.on('message', msg => {
 
-    if (msg.channel.id === env.RELEASE_CHANNEL_ID) {
+	if (msg.channel.id === env.RELEASE_CHANNEL_ID) {
 
-        if (msg.author.bot || msg.content.startsWith('!-')) {
-            return;
-        }
+		if (msg.author.bot || msg.content.startsWith('!-')) {
+			// If message posted by bot or if it starts with '!-', then ignore it
+			return;
+		}
 
-        if (msg.content.toLowerCase().startsWith(cmdPrefix)) {
+		if (msg.content.toLowerCase().startsWith(cmdPrefix)) {
 
-            // Handle commands here
-            const cmd = msg.content.split(' ')[1];
-            const args = msg.content.split(' ').splice(2);
+			let message = msg.content.substr(cmdPrefix.length); // Remove command prefix from message
+			let msgParts = message.split(' ');
 
-            command(cmd, args, msg);
+			// Handle commands here
+			const cmd = msgParts[ 0 ];
+			const args = msgParts.splice(1);
+			const rawArgs = message.substr(cmd.length).trim(); // All arguments but not split
 
-        } else {
+			command(cmd, args, rawArgs, msg);
 
-            if (msg.content.length >= Number(env.MIN_POST_LENGTH) && msg.content.length <= Number(env.MAX_POST_LENGTH)) {
+		} else {
 
-                lastPost = {
-                    msg: msg,
-                    tags: [],
-                    img: ''
-                }
+			if (msg.content.length >= Number(env.MIN_POST_LENGTH) && msg.content.length <= Number(env.MAX_POST_LENGTH)) {
 
-                msg.reply(`New article started.\nAdd tags with \`${cmdPrefix} tags <tag1> <tag2>...\`.\nPost to devRant with \`${cmdPrefix} add\``);
+				const user = getUser(msg);
 
-            } else { // Else, respond with error
+				if (user.newPost.text == '') {
+					db.get('users')
+					  .find({ id: user.id })
+					  .set('newPost.text', msg.content)
+					  .write();
 
-                if (msg.content.length >= Number(env.MIN_POST_LENGTH)) {
+					msg.reply(`new post created!\nAdd tags with \`${cmdPrefix} tags <tag1> <tag2>...\`\nAttach an image with \`${cmdPrefix} image\`\nPost to devRant with \`${cmdPrefix} publish\``);
+				} else {
+					db.get('users')
+					  .find({ id: user.id })
+					  .set('newPost.text', user.newPost.text + "\n\n" + msg.content)
+					  .write();
 
-                    msg.reply('This post seems a little long.');
+					msg.reply('added text to your current post.');
+				}
 
-                } else {
+			} else { // Else, respond with error
 
-                    msg.reply('This post seems a little short.');
+				if (msg.content.length >= Number(env.MIN_POST_LENGTH)) {
+					msg.reply('this post seems a little long.');
+				} else {
+					msg.reply('this post seems a little short.');
+				}
 
-                }
+			}
 
-            }
+		}
 
-        }
-
-    }
+	}
 
 });
 
-function command(cmd: string, args: string[], msg: Discord.Message) {
+function getUser(msg) {
+	let user = db
+		.get('users')
+		.find({ id: msg.author.id })
+		.value();
 
-    switch (cmd) {
+	if (user === undefined) {
+		user = {
+			id:        msg.author.id,
+			signature: '',
+			newPost:   {
+				text:  '',
+				image: '',
+				tags:  []
+			}
+		};
 
-        case 'publish':
-            if (posts.length > 10) {
-                msg.reply('You may not add more articles right now. There are already 10 scheduled for release.');
-                return;
-            }
+		db.get('users')
+		  .push(user)
+		  .write();
+	}
 
-            if (lastPost) {
-                posts.push(lastPost);
-                msg.reply(`"${lastPost.msg.content.substring(0, 15)}..." has been added to the waitlist.`);
-                lastPost = null;
-            } else {
-                msg.reply('No article to publish');
-            }
-            break;
+	return user;
+}
 
-        case 'length':
-            msg.reply(`There are currently ${posts.length} news articles waiting to be posted to devRant.`);
-            break;
+function clearPost(user) {
+	db.get('users')
+	  .find({ id: user.id })
+	  .assign({
+		  newPost: {
+			  text:  '',
+			  image: '',
+			  tags:  []
+		  }
+	  })
+	  .write();
+}
 
-        case 'show':
-            if (args[0] === 'current' && lastPost) {
+function command (cmd: string, args: string[], rawArgs: string, msg: Discord.Message) {
+	const user = getUser(msg);
+	const queue = db.get('queue').value();
 
-                console.log(lastPost.msg.content);
-                msg.reply(`As requested, here is the current article:\n\`\`\`${lastPost.msg.content}\n\nTags: ${lastPost.tags.join(', ')}\nImage: ${lastPost.img}\`\`\``)
+	switch (cmd) {
+		case 'publish':
+			if (queue.length > 10) {
+				msg.reply("you may not add more posts right now.\nThere are already 10 scheduled for release.");
+				return;
+			}
 
-            } else {
+			if (user.newPost.text != '') {
+				db.get('queue')
+				  .push(user.newPost)
+				  .write();
 
-                const post = posts[Number(args[0]) - 1];
-                if (post) {
-                    msg.reply(`As requested, here is article ${Number(args[0])}:\n\`\`\`${post.msg.content}\n\nTags: ${post.tags.join(', ')}\nImage: ${post.img}\`\`\``);
-                } else {
-                    msg.reply('Article does not exist.');
-                }
+				msg.reply(`"${user.newPost.text.substring(0, 15)}..." has been added to the release-queue.`);
 
-            }
-            break;
+				clearPost(user);
+			} else {
+				msg.reply("you don't have a post to publish.");
+			}
+			break;
 
-        case 'tags':
-            if (lastPost) {
-                lastPost.tags = args.filter(t => t.trim()).map(t => t.replace(',', ''));
-                msg.reply(`Added tags: ${lastPost.tags.join(', ')}.`);
-            } else {
-                msg.reply('Article does not exist.');
-            }
-            break;
+		case 'length':
+			msg.reply(`there are currently **${queue.length}** post(s) waiting to be published to devRant.`);
+			break;
 
-        case 'image':
-            if (lastPost) {
-                if (msg.attachments.array().length > 0) {
-                    console.log(msg.attachments.array()[0].url);
-                    lastPost.img = msg.attachments.array()[0].url;
-                    console.log(lastPost.img);
-                    msg.reply('Image attatched');
-                } else {
-                    msg.reply('No image attatched.');
-                }
-            } else {
-                msg.reply('Article does not exist.');
-            }
-            break;
+		case 'show':
+			if (args[ 0 ] === 'current' && user.newPost.text != '') {
 
-        case 'reset':
-            lastPost = null;
-            msg.reply('Current article deleted.');
-            break;
+				console.log(user.newPost.text);
+				msg.reply(`as requested, here is your current post:\n\`\`\`${user.newPost.text}\n------\nTags: ${user.newPost.tags.join(', ')}\nImage: ${user.newPost.image}\`\`\``);
 
-        case 'help':
-            msg.reply(helpText());
-            break
+			} else {
+				const post = queue[ Number(args[ 0 ]) - 1 ];
 
-        default:
-            msg.reply(`Unknown command. \`${cmdPrefix} help\` for more help.`);
+				if (post) {
+					msg.reply(`as requested, here is post #${Number(args[ 0 ])}:\n\`\`\`${post.text}\n------\nTags: ${post.tags.join(', ')}\nImage: ${post.image}\`\`\``);
+				} else {
+					msg.reply('post does not exist.');
+				}
 
-    }
+			}
+			break;
+
+		case 'tags':
+			if (user.newPost.text != '') {
+				const newTags = rawArgs.split(',').map(t => t.trim());
+
+				db.get('users')
+				  .find({ id: user.id })
+				  .set('newPost.tags', newTags)
+				  .write();
+
+				msg.reply(`added tags: \`${newTags.join(', ')}\``);
+			} else {
+				msg.reply('there is no post to add tags to.');
+			}
+			break;
+
+		case 'image':
+			if (user.newPost.text != '') {
+				if (msg.attachments.array().length > 0) {
+					console.log(msg.attachments.array()[ 0 ].url);
+
+					db.get('users')
+					  .find({ id: user.id })
+					  .set('newPost.image', msg.attachments.array()[ 0 ].url)
+					  .write();
+
+					msg.reply('attached image.');
+				} else {
+					msg.reply('no image attatched.');
+				}
+			} else {
+				msg.reply('there is no post to attach the image to.');
+			}
+			break;
+
+		case 'reset':
+			clearPost(user);
+
+			msg.reply('current post deleted.');
+			break;
+
+		case 'signature':
+			const newSignature = rawArgs;
+
+			if (newSignature != '') {
+				db.get('users')
+				  .find({ id: user.id })
+				  .set('signature', newSignature)
+				  .write();
+
+				msg.reply(`set signature to:\n\`\`\`${newSignature}\`\`\``);
+			} else {
+				if (user.signature != '') {
+					msg.reply(`your current signature:\n\`\`\`${user.signature}\`\`\``);
+				} else {
+					msg.reply('you didn\'t set a signature yet!');
+				}
+			}
+			break;
+
+		case 'help':
+			msg.reply(helpText(queue));
+			break;
+
+		default:
+			msg.reply(`Unknown command. \`${cmdPrefix}help\` for more help.`);
+
+	}
 
 }
 
 // Connect to discord
 client.login(env.DISCORD_TOKEN)
-    .catch(e => console.error(e));
+      .catch(e => console.error(e));
 
 // Set interval
 console.log(`POST_FREQUENCY set to ${env.POST_FREQUENCY || 10} minutes.`);
 setInterval(() => {
+	const queue = db.get('queue').value();
 
-    if (posts.length > 0) {
+	if (queue.length > 0) {
 
-        const post = posts[0];
-        console.log(post.msg.content);
+		const post = queue[ 0 ];
+		console.log(post.text);
 
-        // Get authentication token from devRant API
-        // rantscript
-        //     .login(String(env.DEVRANT_USENAME), String(env.DEVRANT_PASSWORD))
-        //     .then(response => {
-        //         rantscript.postRant(
-        //             post.msg.content,
-        //             post.tags.join(','),
-        //             Number(env.DEVRANT_POST_CATEGORY || 6),
-        //             response.auth_token,
-        //             null
-        //         ).then(resp => {
-        //             posts.shift();
-        //             sendMessage(`Posted article to devRant! Rant ID: ${resp.rant_id}.`);
-        //         }).catch(e => {
-        //             console.error(e);
-        //             sendMessage('Something went wrong when posting article to devRant!');
-        //         });
-        //     }).catch(e => {
-        //         console.error(e);
-        //         sendMessage('Something went wrong when posting article to devRant!');
-        //     });
+		// Get authentication token from devRant API
+		// rantscript
+		//     .login(String(env.DEVRANT_USERNAME), String(env.DEVRANT_PASSWORD))
+		//     .then(response => {
+		//         rantscript.postRant(
+		//             post.msg.content,
+		//             post.tags.join(','),
+		//             Number(env.DEVRANT_POST_CATEGORY || 6),
+		//             response.auth_token,
+		//             null
+		//         ).then(resp => {
+		//             posts.shift();
+		//             sendMessage(`Posted article to devRant! Rant ID: ${resp.rant_id}.`);
+		//         }).catch(e => {
+		//             console.error(e);
+		//             sendMessage('Something went wrong when posting article to devRant!');
+		//         });
+		//     }).catch(e => {
+		//         console.error(e);
+		//         sendMessage('Something went wrong when posting article to devRant!');
+		//     });
 
-    }
+	}
 
 }, Number(env.POST_FREQUENCY || 10) * 60 * 1000);
