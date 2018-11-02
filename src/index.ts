d81b8cdcf2c07fe3706a9cc83bcb807ecbe324a6
Skayo <jonasg.cool@gmail.com>
2018-11-02 16:08:51 +0100

    Added posting to devRant

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import * as Discord from 'discord.js';
+import * as fs from 'fs';
 import { env } from 'process';
 import * as dotenv from 'dotenv';
-import * as rantscript from 'rantscript';
+import * as Discord from 'discord.js';
+import * as devRant from 'rantscript';
 import * as lowdb from 'lowdb';
 import * as FileSync from 'lowdb/adapters/FileSync';
-
-interface Post {
-	msg: Discord.Message;
-	tags: string[];
-	img: string;
-}
+import * as download from 'download-to-file';
 
 dotenv.load({ path: '.env' }); // Load .env
 
-const cmdPrefix = env.COMMAND_PREFIX || '!dnb';
+const cmdPrefix = env.COMMAND_PREFIX || '!';
 const client = new Discord.Client();
+
+if (!fs.existsSync('./temp')) {
+	fs.mkdirSync('./temp');
+}
 
 const adapter = new FileSync('db.json');
 const db = lowdb(adapter);
@@ -37,19 +37,6 @@ Re-try / Post frequency: ${env.POST_FREQUENCY || 10} minutes.
 Articles in queue: ${queue.length} (max: 10).
 `;
 }
-
-/*
-function sendMessage(msg: string) {
-
-    const channel = client.channels.get(env.RELEASE_CHANNEL_ID || '');
-
-    if (channel) {
-        // @ts-ignore
-        channel.send(msg);
-    }
-
-}
-*/
 
 
 client.on('ready', () => {
@@ -89,7 +76,7 @@ client.on('message', msg => {
 			let msgParts = message.split(' ');
 
 			// Handle commands here
-			const cmd = msgParts[ 0 ];
+			const cmd = msgParts[0];
 			const args = msgParts.splice(1);
 			const rawArgs = message.substr(cmd.length).trim(); // All arguments but not split
 
@@ -107,11 +94,11 @@ client.on('message', msg => {
 					  .set('newPost.text', msg.content)
 					  .write();
 
-					msg.reply(`new post created!\nAdd tags with \`${cmdPrefix} tags <tag1> <tag2>...\`\nAttach an image with \`${cmdPrefix} image\`\nPost to devRant with \`${cmdPrefix} publish\``);
+					msg.reply(`new post created!\nAdd tags with \`${cmdPrefix}tags <tag1> <tag2>...\`\nAttach an image with \`${cmdPrefix}image\`\nPost to devRant with \`${cmdPrefix}publish\``);
 				} else {
 					db.get('users')
 					  .find({ id: user.id })
-					  .set('newPost.text', user.newPost.text + "\n\n" + msg.content)
+					  .set('newPost.text', user.newPost.text + '\n\n' + msg.content)
 					  .write();
 
 					msg.reply('added text to your current post.');
@@ -133,7 +120,7 @@ client.on('message', msg => {
 
 });
 
-function getUser(msg) {
+function getUser (msg) {
 	let user = db
 		.get('users')
 		.find({ id: msg.author.id })
@@ -158,7 +145,7 @@ function getUser(msg) {
 	return user;
 }
 
-function clearPost(user) {
+function clearPost (user) {
 	db.get('users')
 	  .find({ id: user.id })
 	  .assign({
@@ -178,11 +165,13 @@ function command (cmd: string, args: string[], rawArgs: string, msg: Discord.Mes
 	switch (cmd) {
 		case 'publish':
 			if (queue.length > 10) {
-				msg.reply("you may not add more posts right now.\nThere are already 10 scheduled for release.");
+				msg.reply('you may not add more posts right now.\nThere are already 10 scheduled for release.');
 				return;
 			}
 
 			if (user.newPost.text != '') {
+				user.newPost.userID = user.id; // Add user ID to newPost so we know who made this post once in the queue
+
 				db.get('queue')
 				  .push(user.newPost)
 				  .write();
@@ -190,8 +179,10 @@ function command (cmd: string, args: string[], rawArgs: string, msg: Discord.Mes
 				msg.reply(`"${user.newPost.text.substring(0, 15)}..." has been added to the release-queue.`);
 
 				clearPost(user);
+
+				tryPost(); // Try to post it
 			} else {
-				msg.reply("you don't have a post to publish.");
+				msg.reply('you don\'t have a post to publish.');
 			}
 			break;
 
@@ -200,16 +191,15 @@ function command (cmd: string, args: string[], rawArgs: string, msg: Discord.Mes
 			break;
 
 		case 'show':
-			if (args[ 0 ] === 'current' && user.newPost.text != '') {
+			if (args[0] === 'current' && user.newPost.text != '') {
 
-				console.log(user.newPost.text);
 				msg.reply(`as requested, here is your current post:\n\`\`\`${user.newPost.text}\n------\nTags: ${user.newPost.tags.join(', ')}\nImage: ${user.newPost.image}\`\`\``);
 
 			} else {
-				const post = queue[ Number(args[ 0 ]) - 1 ];
+				const post = queue[Number(args[0]) - 1];
 
 				if (post) {
-					msg.reply(`as requested, here is post #${Number(args[ 0 ])}:\n\`\`\`${post.text}\n------\nTags: ${post.tags.join(', ')}\nImage: ${post.image}\`\`\``);
+					msg.reply(`as requested, here is post #${Number(args[0])}:\n\`\`\`${post.text}\n------\nTags: ${post.tags.join(', ')}\nImage: ${post.image}\`\`\``);
 				} else {
 					msg.reply('post does not exist.');
 				}
@@ -235,11 +225,11 @@ function command (cmd: string, args: string[], rawArgs: string, msg: Discord.Mes
 		case 'image':
 			if (user.newPost.text != '') {
 				if (msg.attachments.array().length > 0) {
-					console.log(msg.attachments.array()[ 0 ].url);
+					console.log(msg.attachments.array()[0].url);
 
 					db.get('users')
 					  .find({ id: user.id })
-					  .set('newPost.image', msg.attachments.array()[ 0 ].url)
+					  .set('newPost.image', msg.attachments.array()[0].url)
 					  .write();
 
 					msg.reply('attached image.');
@@ -289,40 +279,102 @@ function command (cmd: string, args: string[], rawArgs: string, msg: Discord.Mes
 
 // Connect to discord
 client.login(env.DISCORD_TOKEN)
-      .catch(e => console.error(e));
+      .then(() => tryPost())
+      .catch(console.error);
 
-// Set interval
-console.log(`POST_FREQUENCY set to ${env.POST_FREQUENCY || 10} minutes.`);
-setInterval(() => {
+function tryPost () {
 	const queue = db.get('queue').value();
 
 	if (queue.length > 0) {
+		const post = queue[0];
 
-		const post = queue[ 0 ];
-		console.log(post.text);
+		const channel: Discord.Channel | undefined = client.channels.get(env.RELEASE_CHANNEL_ID || '');
+
+		if (channel === undefined) {
+			console.error('Channel with ID ' + env.RELEASE_CHANNEL_ID + ' not found. Please check!');
+		}
 
 		// Get authentication token from devRant API
-		// rantscript
-		//     .login(String(env.DEVRANT_USERNAME), String(env.DEVRANT_PASSWORD))
-		//     .then(response => {
-		//         rantscript.postRant(
-		//             post.msg.content,
-		//             post.tags.join(','),
-		//             Number(env.DEVRANT_POST_CATEGORY || 6),
-		//             response.auth_token,
-		//             null
-		//         ).then(resp => {
-		//             posts.shift();
-		//             sendMessage(`Posted article to devRant! Rant ID: ${resp.rant_id}.`);
-		//         }).catch(e => {
-		//             console.error(e);
-		//             sendMessage('Something went wrong when posting article to devRant!');
-		//         });
-		//     }).catch(e => {
-		//         console.error(e);
-		//         sendMessage('Something went wrong when posting article to devRant!');
-		//     });
+		devRant.login(
+			String(env.DEVRANT_USERNAME),
+			String(env.DEVRANT_PASSWORD)
+		).then(tokenData => {
+			const postRant = function (filePath = false) {
+				// Post rant
+				devRant.postRant(
+					post.text,
+					post.tags.join(', '),
+					Number(env.DEVRANT_POST_CATEGORY || 6),
+					tokenData.auth_token,
+					(filePath || null)
+				).then(postData => {
+					if (postData.success) {
+						// Delete post in queue
+						db.get('queue')
+						  .shift()
+						  .write();
+
+						channel.send(`Posted article to devRant!\nLink: https://devrant.com/rants/${postData.rant_id}`)
+						       .catch(console.error);
+
+						postSignature(post.userID, postData.rant_id, tokenData.auth_token);
+					} else if (!postData.error.startsWith('Right now you can only add')) {
+						db.get('queue')
+						  .shift()
+						  .write();
+
+						channel.send('Something went wrong while posting to devRant:\n```\n' + postData.error + '\n```\nPost got removed from the release-queue. Please publish it again!')
+						       .catch(console.error);
+					}
+
+				}).catch(console.error);
+			};
+
+			if (post.image != '') {
+				downloadFile(post.image, post.userID, function (err, filePath) {
+					if (err) console.error(err);
+
+					postRant(filePath);
+				});
+			} else {
+				postRant();
+			}
+		}).catch(() => {
+			console.error('Unable to log-in to devRant! Please check username/password!');
+
+			channel.send('Unable to log-in to devRant! Please check username/password!')
+			       .catch(console.error);
+		});
 
 	}
+}
 
-}, Number(env.POST_FREQUENCY || 10) * 60 * 1000);
+// Download a file and return it's local file path
+function downloadFile (url, userID, callback) {
+	if (url == '') {
+		return false;
+	}
+
+	const fileExtension = url.split('.').pop().toLowerCase();
+	const filePath = './temp/' + userID + '.' + fileExtension;
+
+	if (!['png', 'jpg', 'jpeg', 'gif'].includes(fileExtension)) {
+		return false;
+	}
+
+	return download(url, filePath, callback);
+}
+
+function postSignature (userID, rantID, token) {
+	const signature = db
+		.get('users')
+		.find({ id: userID })
+		.get('signature')
+		.value();
+
+	devRant.postComment(signature, rantID, token)
+	       .catch(console.error);
+}
+
+// Execute tryPost every x minutes
+setTimeout(tryPost, Number(env.POST_FREQUENCY || 10) * 60 * 1000);
